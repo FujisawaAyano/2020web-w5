acf9c35984b5792dda0af3824dab61e71287236a
FujisawaAyano <75403424+FujisawaAyano@users.noreply.github.com>
2020-12-30 16:28:27 +0900

    Update main-ja.js

--- main-ja.js
+++ main-ja.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = '気温が華氏94度もあったので:insertx:たちは散歩に出かけました。彼らが:inserty:に到着した時、彼らはとても驚きました。なぜなら:insertz:。しかしボブはそれを見ても驚くことはありませんでした。それは:inserty:ではよく起きる事だからです。';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = '外は華氏94度もあったので:insertx:たちは散歩に出かけました。彼らが:inserty:に到着した時、彼らはとても驚きました。なぜなら:insertz:からです。しかしボブはそれを見ても驚くことはありませんでした。それは:inserty:ではよく起きる事だからです。';
+let insertX = ['くまモン','ふなっしー','ひこにゃん'];
+let insertY = ['熊本城','ディズニーランド','彦根城'];
+let insertZ = ['宇宙人に会った','流れ星を見た','魔法を使えるようになった'];
 
 randomize.addEventListener('click', result);
 
