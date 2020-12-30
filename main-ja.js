2656add54dd05f92fe9aed2f9593bcbabd3a15e7
FujisawaAyano <75403424+FujisawaAyano@users.noreply.github.com>
2020-12-30 16:04:34 +0900

    Update main-ja.js

--- main-ja.js
+++ main-ja.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = '外は華氏94度もあったので:insertx:は散歩に出かけました。彼らが:inserty:に到着しそれを見た時、彼らはとても驚き:insertz:。しかしボブはそれを見ても驚くことはありませんでした。なぜなら:insertx:はとても重かったからです。';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
